0ebf83f2366009efc22ecc8f468a5cdd713291fe
NikitaKloch <yan1squadd@gmail.com>
2022-04-23 00:46:40 +0300

    programma

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+ 
+using namespace std;
+ 
+main()
+{
+    int a,b,c,S;
+    cout<<"Введите первый катет: ";
+    cin>>a;
+    cout<<"Введите второй катет: ";
+    cin>>b;
+    c=sqrt(a*a+b*b);
+    S=a*b/2;
+    cout<<"Гипотенуза равна: "<< c << ", Площадь равна: "<< S;
+    return 0;
+}
